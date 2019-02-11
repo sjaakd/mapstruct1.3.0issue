8d31f4b2ff0790e015543f37b83e86f45737df8d
Michael Risthaus <mristhaus@gmail.com>
2019-02-11 18:35:39 +0100

    adding example sources

---
+++ src/main/java/com/wigball/mapstruct/issue/model/TargetClass.java
@@ -0,0 +1,21 @@
+package com.wigball.mapstruct.issue.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Target class to which mapstruct should map. Enriched by lombok.
+ */
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Builder
+public class TargetClass {
+
+    private String aString;
+    private Boolean aFlag;
+    private Boolean anotherFlag;
+
+}
